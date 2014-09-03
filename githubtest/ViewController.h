0c2f4b4b1a2386835c42e667b93d907f3d722274
植田 大貴 <uedadaiki@ueda-no-MacBook-Air.local>
2014-09-03 14:46:44 +0900

    Initial Commit

---
+++ githubtest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  githubtest
+//
+//  Created by 植田 大貴 on 2014/09/03.
+//  Copyright (c) 2014年 植田 大貴. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ githubtest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  githubtest
+//
+//  Created by 植田 大貴 on 2014/09/03.
+//  Copyright (c) 2014年 植田 大貴. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
